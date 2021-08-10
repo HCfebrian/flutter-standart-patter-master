301f55455620445ae8f05f32619c5192ca26feba
Heryan CF <heryan@tlab.co.id>
2021-08-10 17:28:11 +0700

    add example on localization

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:simple_flutter/core/constant/static_constant.dart';
 import 'package:simple_flutter/feature/splash_screen/presentation/splash_screen.dart';
+import 'package:simple_flutter/translations/codegen_loader.g.dart';
 
 Future<void> mainInit() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +10,7 @@ Future<void> mainInit() async {
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en')],
+      assetLoader: CodegenLoader(),
       path: 'assets/translations',
       fallbackLocale: Locale('en'),
       child: MainApp(),
