b3fa8a29c6dc0707f7976b2be7a85e5cedf2cee0
Heryan CF <heryan@tlab.co.id>
2021-08-10 11:09:54 +0700

    add Auth bloc to perform auth activity

---
+++ lib/feature/auth/presentation/bloc/auth/auth_state.dart
@@ -0,0 +1,26 @@
+part of 'auth_bloc.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {
+  @override
+  List<Object> get props => [];
+}
+
+class AuthLoadingState extends AuthState {}
+
+class AuthSuccessState extends AuthState {}
+
+class AuthErrorState extends AuthState {
+  final String message;
+
+  AuthErrorState({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
