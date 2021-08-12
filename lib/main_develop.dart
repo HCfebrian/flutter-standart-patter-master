6d067e83276408e08e714b25c82b577783581c50
HCfebrian <febriansyah.online@gmail.com>
2021-08-12 11:04:08 +0700

    format code

--- lib/main_develop.dart
+++ lib/main_develop.dart
@@ -5,7 +5,6 @@ import 'package:simple_flutter/core/constant/static_constant.dart';
 import 'package:simple_flutter/main.dart';
 import 'package:simple_flutter/utils/flavour_config.dart';
 
-
 void main() {
   FlavorConfig(
       flavor: Flavor.DEVELOP, //Flavor Type
